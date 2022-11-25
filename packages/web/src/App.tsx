e3463304047f66b1828349b551eb39557f802176
Vincent <thisoneis4business@gmail.com>
2022-11-26 01:33:46 +1300

    feat(web|server): Fixed cors + useFetch

--- packages/web/src/App.tsx
+++ packages/web/src/App.tsx
@@ -5,50 +5,44 @@
 //  Created by d-exclaimation on 25 Nov 2022
 //
 
-import { FC, useRef, useState } from "react";
-import { useMount } from "./hooks/useMount";
+import { FC, useState } from "react";
 
 const App: FC = () => {
   const [count, setCount] = useState(0);
-  const countRef = useRef(0);
-
-  useMount(() => {
-    countRef.current++;
-    console.log(countRef.current);
-  });
-
   return (
-    <div className="flex items-center justify-center w-screen h-screen bg-slate-800">
-      <button
-        className="
+    <div className="flex flex-col items-center justify-center w-screen h-screen bg-slate-800">
+      <div className="flex items-center justify-center">
+        <button
+          className="
           w-20 h-12 rounded-l-lg text-lg font-mono
           text-white bg-teal-700
           hover:bg-teal-600
           active:bg-teal-600 active:scale-95
         "
-        onClick={() => setCount((count) => count + 1)}
-      >
-        +
-      </button>
-      <button
-        className="
+          onClick={() => setCount((count) => count + 1)}
+        >
+          +
+        </button>
+        <button
+          className="
           w-28 h-12 font-mono
           text-white bg-slate-700
         "
-      >
-        {count}
-      </button>
-      <button
-        className="
+        >
+          {count}
+        </button>
+        <button
+          className="
           w-20 h-12 rounded-r-lg text-lg font-mono
           text-white bg-rose-700
           hover:bg-rose-600
           active:bg-rose-600 active:scale-95
         "
-        onClick={() => setCount((count) => count - 1)}
-      >
-        -
-      </button>
+          onClick={() => setCount((count) => count - 1)}
+        >
+          -
+        </button>
+      </div>
     </div>
   );
 };
