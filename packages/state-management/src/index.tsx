8f24da698013bce2c4e312102f0861b8f8a059df
Vincent <thisoneis4business@gmail.com>
2022-11-27 00:31:04 +1300

    feat(state-m): Created initial state management

---
+++ packages/state-management/src/index.tsx
@@ -0,0 +1,9 @@
+//
+//  index.tsx
+//  dino-league
+//
+//  Created by d-exclaimation on 27 Nov 2022
+//
+
+export { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
+export * from "./apollo/http";
