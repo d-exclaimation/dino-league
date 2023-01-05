aa840772f72027d188286e7a210c923d46e3187f
Vincent <thisoneis4business@gmail.com>
2023-01-05 16:33:44 +1300

    feat(web): Better GUI for battles

--- packages/web/src/pages/pve/index.tsx
+++ packages/web/src/pages/pve/index.tsx
@@ -15,6 +15,8 @@ import {
 } from "@dino/apollo";
 import { FC, useCallback, useMemo, useRef, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
+import { Color } from "../../common/Styling";
 
 type BattleDinoDisplay = {
   damage?: number;
@@ -33,6 +35,21 @@ type BattlePlan =
   | (Omit<BattleInit, "yours" | "opponents"> & Battling)
   | (Omit<BattleTurn, "yours" | "opponents"> & Battling);
 
+const damageColor = (num: number, hp: number): Color.Bg => {
+  const percentage = Math.round((num * 100) / hp);
+  if (percentage < 25) return "bg-sky-400";
+  if (percentage < 50) return "bg-green-400";
+  if (percentage < 75) return "bg-amber-400";
+  return "bg-red-400";
+};
+
+const hpColor = (percentage: number): Color.Text => {
+  if (percentage < 33) return "text-red-500";
+  if (percentage < 67) return "text-amber-500";
+  return "text-green-500";
+};
+
+// TODO: Reorganise into components and reusable hooks
 const PvEPage: FC = () => {
   const nav = useNavigate();
   const { user } = useAuth();
@@ -55,6 +72,12 @@ const PvEPage: FC = () => {
       const [head, ...tail] = plan;
       switch (head.__typename) {
         case "BattleEnd":
+          toast(head.win ? "🎉 Quest completed" : "🔥 Quest failed", {
+            type: head.win ? "default" : "error",
+            autoClose: 2000,
+            pauseOnHover: true,
+            theme: "light",
+          });
           return;
         case "BattleInit":
           setYours((prev) => {
@@ -78,7 +101,7 @@ const PvEPage: FC = () => {
           timeoutRef.current = setTimeout(() => applyEvent(tail), 500);
           break;
         case "BattleTurn":
-          const { attacking, yours, opponents } = head;
+          const { attacking, yours, opponents, damage } = head;
 
           timeoutRef.current = setTimeout(() => {
             if (timeoutRef.current) clearTimeout(timeoutRef.current);
@@ -86,6 +109,7 @@ const PvEPage: FC = () => {
             setYours((prev) => {
               const newState = {
                 dino: yours,
+                damage: !attacking ? damage : undefined,
                 attacking,
               };
               return prev ? { ...prev, ...newState } : prev;
@@ -93,15 +117,18 @@ const PvEPage: FC = () => {
             setOpponents((prev) => {
               const newState = {
                 dino: opponents,
+                damage: attacking ? damage : undefined,
                 attacking: !attacking,
               };
               return prev ? { ...prev, ...newState } : prev;
             });
 
             timeoutRef.current = setTimeout(() => {
-              setYours((prev) => (prev ? { ...prev, attacking: false } : prev));
+              setYours((prev) =>
+                prev ? { ...prev, attacking: false, damage: undefined } : prev
+              );
               setOpponents((prev) =>
-                prev ? { ...prev, attacking: false } : prev
+                prev ? { ...prev, attacking: false, damage: undefined } : prev
               );
 
               applyEvent(tail);
@@ -129,26 +156,45 @@ const PvEPage: FC = () => {
   }, [quest, applyEvent]);
 
   return (
-    <div
-      className="flex items-center justify-center w-screen h-screen bg-cover"
-      style={{
-        backgroundImage: `url('/bg/${bgImg}')`,
-      }}
-    >
+    <div className="flex items-center justify-center w-screen h-screen bg-gradient-to-t from-white to-[#C0B2A2]">
       <button className="fixed top-2 right-2" onClick={() => nav("/")}>
         <img className="w-10 md:w-12" src="/back.svg" />
       </button>
-      <div className="flex flex-col items-center justify-center max-w-3xl h-1/2 w-[90%] bg-white/20 rounded-lg">
+      <div
+        className="flex flex-col items-center justify-center max-w-3xl h-1/2 w-[90%] bg-cover rounded-lg"
+        style={{
+          backgroundImage: `url('/bg/${bgImg}')`,
+        }}
+      >
         <div className="flex items-center justify-between px-5 py-2 w-full h-4/6">
           {yours ? (
-            <img
-              className={`w-24 md:w-40 transition ease-out duration-300 ${
-                yours.attacking
-                  ? "translate-x-[min(30rem,45vw)] scale-110 z-10"
-                  : ""
-              } ${yours.dino.hp <= 0 ? "contrast-50 brightness-[3]" : ""}`}
-              src={`/${yours.dino.variant.toLowerCase()}.gif`}
-            />
+            <div className="flex items-center justify-between flex-col h-full">
+              <span
+                className={`text-white ${
+                  yours.damage
+                    ? `p-1 ${damageColor(yours.damage, yours.dino.hp)}`
+                    : ""
+                }  text-xs md:text-base font-medium rounded-md`}
+              >
+                {yours.damage?.toFixed(2) ?? ""}
+              </span>
+              <img
+                className={`w-24 md:w-40 transition ease-out duration-300 ${
+                  yours.attacking
+                    ? "translate-x-[min(30rem,45vw)] scale-110 z-10"
+                    : ""
+                } ${yours.dino.hp <= 0 ? "contrast-50 brightness-[3]" : ""}`}
+                src={`/${yours.dino.variant.toLowerCase()}.gif`}
+              />
+              <span
+                className={
+                  "bg-white py-1 px-4 md:text-lg font-medium rounded-md " +
+                  hpColor(yours.dino.percentage)
+                }
+              >
+                {yours.dino.hp.toFixed(2)}
+              </span>
+            </div>
           ) : (
             <img
               className="w-24 md:w-40 transition ease-out duration-300 contrast-0 brightness-[3] animate-pulse"
@@ -168,14 +214,35 @@ const PvEPage: FC = () => {
             </button>
           )}
           {opponents ? (
-            <img
-              className={`w-24 md:w-40 transition ease-out duration-300 -scale-x-100 ${
-                opponents.attacking
-                  ? "-translate-x-[min(30rem,45vw)] scale-110 z-10"
-                  : ""
-              } ${opponents.dino.hp <= 0 ? "contrast-50 brightness-[3]" : ""}`}
-              src={`/${opponents.dino.variant.toLowerCase()}.gif`}
-            />
+            <div className="flex items-center justify-between flex-col h-full">
+              <span
+                className={`text-white ${
+                  opponents.damage
+                    ? `p-1 ${damageColor(opponents.damage, opponents.dino.hp)}`
+                    : ""
+                }  text-xs md:text-base font-medium rounded-md`}
+              >
+                {opponents.damage?.toFixed(2) ?? ""}
+              </span>
+              <img
+                className={`w-24 md:w-40 transition ease-out duration-300 -scale-x-100 ${
+                  opponents.attacking
+                    ? "-translate-x-[min(30rem,45vw)] scale-110 z-10"
+                    : ""
+                } ${
+                  opponents.dino.hp <= 0 ? "contrast-50 brightness-[3]" : ""
+                }`}
+                src={`/${opponents.dino.variant.toLowerCase()}.gif`}
+              />
+              <span
+                className={
+                  "bg-white py-1 px-4 md:text-lg font-medium rounded-md " +
+                  hpColor(opponents.dino.percentage)
+                }
+              >
+                {opponents.dino.hp.toFixed(2)}
+              </span>
+            </div>
           ) : (
             <img
               className="w-24 md:w-40 transition ease-out duration-300 contrast-0 brightness-[3] -scale-x-100 animate-pulse"
