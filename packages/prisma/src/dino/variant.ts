aba63b5dc9fedf72911a5fb0eb916f134738a1a9
Vincent <thisoneis4business@gmail.com>
2023-01-08 23:17:22 +1300

    fix(prisma): Reduced pricing

--- packages/prisma/src/dino/variant.ts
+++ packages/prisma/src/dino/variant.ts
@@ -86,7 +86,9 @@ export function adjusted(
   level: number
 ) {
   const scale = Math.pow(1.01, level - 1);
-  const price = 3 * (hp / 100 + attack / 50 + speed / 100 + healing / 40) * 100;
+  const price = Math.round(
+    3 * (hp / 100 + attack / 50 + speed / 100 + healing / 40) * 40
+  );
   return {
     hp: scale * hp,
     attack: scale * attack,
